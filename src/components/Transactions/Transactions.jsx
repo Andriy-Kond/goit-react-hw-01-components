92925543550c8f22580fa9aeb3e154c7b5745071
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-16 10:08:56 +0200

    05

---
+++ src/components/Transactions/Transactions.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import css from './Transactions.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.TransactionHistory}>
+      <thead>
+        <tr className={css.TransactionRow}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} className={css.TransactionRow}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
