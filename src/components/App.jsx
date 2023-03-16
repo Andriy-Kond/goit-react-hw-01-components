8ff586b6c3f4adc14fb628ee9a873b17429951ee
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-16 09:49:40 +0200

    02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import user from './Profile/user.json';
+import { ProfileArr } from './Profile/Profile';
+
+import data from './Statistics/data.json';
+import { Statistic } from './Statistics/Statistics';
+
+import friends from './FriendList/friends.json';
+import { FriendList } from './FriendList/FriendList';
+
+import transactions from './Transactions/transactions.json';
+import { TransactionHistory } from './Transactions/Transactions.jsx';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <ProfileArr
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistic title="UPLOAD STATES" dataArr={data} />;
+      {/* <Statistic dataArr={data} />; */}
+      <FriendList propFriendsArr={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
-};
+}
